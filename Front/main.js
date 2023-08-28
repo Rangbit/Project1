95232b1b9f8ef0766fcfb9d978b272ba3f11f8d0
랑빛 <Rangbits@gmail.com>
2023-08-28 19:06:59 +0900

    230828 study

---
+++ Front/main.js
@@ -0,0 +1,14 @@
+let cnt = 0;
+function sideMenu() {
+    let element_bar = document.getElementsByClassName("main-menu__side-bar")[0];
+    console.log(element_bar);         
+    if(cnt%2==1){
+        element_bar.classList.add('close_menu-ani');
+        element_bar.classList.remove('open_menu-ani');
+    }else if(cnt%2==0){
+        element_bar.classList.add('open_menu-ani');
+        element_bar.classList.remove('close_menu-ani');
+    }
+    cnt += 1;
+    console.log(element_bar.className);
+}
